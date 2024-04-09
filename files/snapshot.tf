97e368702cbc1d05f708a82e49e7d34343c5887d
PavelKondakov22 <146209575+PavelKondakov22@users.noreply.github.com>
2024-04-09 13:56:55 +0300

    Update snapshot.tf

--- files/snapshot.tf
+++ files/snapshot.tf
@@ -14,6 +14,10 @@ resource "yandex_compute_snapshot_schedule" "mysnapshot" {
 
   retention_period = "168h"
 
+  labels = {
+    my-label = "my-label-value"
+  }
+
   disk_ids = [ yandex_compute_instance.nginx-1.boot_disk.0.disk_id, yandex_compute_instance.nginx-2.boot_disk.0.disk_id, yandex_compute_instance.bastion.boot_disk.0.disk_id, yandex_compute_instance.elastic.boot_disk.0.disk_id, yandex_compute_instance.kibana.boot_disk.0.disk_id, yandex_compute_instance.zabbix.boot_disk.0.disk_id]
            
 }
